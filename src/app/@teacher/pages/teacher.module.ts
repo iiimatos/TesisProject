1df310ad10963cdb3bd69263b4d77ecece3578d5
Rafael Espinal <respinal1200@gmail.com>
2021-10-11 18:58:52 -0400

    Admin teacher listo

--- src/app/@teacher/pages/teacher.module.ts
+++ src/app/@teacher/pages/teacher.module.ts
@@ -3,11 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { TeacherRoutingModule } from './teacher-routing.module';
 import { TeacherComponent } from './teacher.component';
+import { NavbarComponent } from '../core/components/navbar/navbar.component';
+import { SidebarComponent } from '../core/components/sidebar/sidebar.component';
+import { AsideComponent } from '../core/components/aside/aside.component';
+import { FooterComponent } from '../core/components/footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    TeacherComponent
+    TeacherComponent,
+    NavbarComponent,
+    SidebarComponent,
+    AsideComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule,
