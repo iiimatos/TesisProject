9a4fede966e117a73b74b157f5c2c17127cba0bc
Rafael Espinal <respinal1200@gmail.com>
2021-10-15 22:53:25 -0400

    Cambios

--- src/app/@teacher/pages/teacher.component.ts
+++ src/app/@teacher/pages/teacher.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./teacher.component.scss']
 })
 export class TeacherComponent implements OnInit {
-  name = 'Ivan Matos';
+  name = 'Alexander Pierce';
   constructor() { }
 
   ngOnInit(): void {
